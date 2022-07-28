5ff64d1b96823cd2c1dd00c8c52de97f1801097c
pjdidonatojr <pjdidonatojr@gmail.com>
2022-07-27 22:32:33 -0400

    rwerw

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ app.use(express.static('./ar-app'))
 
 app.use([logger, authorize])
 // api/home/about/products
-app.get('/', (req, res) => {
- res.send('Home')
-})
-app.get('/about', (req, res) => {
- res.send('About')
-})
-app.get('/api/products', (req, res) => {
- res.send('Products')
-})
-app.get('/api/items', (req, res) => {
- res.send('Items')
-})
+// app.get('/', (req, res) => {
+//  res.send('Home')
+// })
+// app.get('/about', (req, res) => {
+//  res.send('About')
+// })
+// app.get('/api/products', (req, res) => {
+//  res.send('Products')
+// })
+// app.get('/api/items', (req, res) => {
+//  res.send('Items')
+// })
 app.listen(5000, () => {
  console.log('Server is listening on port 5000.....')
 })
